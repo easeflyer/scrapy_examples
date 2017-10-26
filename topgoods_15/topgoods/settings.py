1b83367a1498803de49248a1b07884bf3bf41ba0
jian zhang <jian.zhang@fuwo.com>
2017-10-26 17:35:20 +0800

    change package name;

--- topgoods_15/topgoods/settings.py
+++ topgoods_15/topgoods/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for topgoods_11 project
+# Scrapy settings for topgoods_15 project
 #
 # For simplicity, this file contains only the most important settings by
 # default. All the other settings are documented here:
@@ -8,10 +8,10 @@
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
 
-BOT_NAME = 'topgoods_11'
+BOT_NAME = 'topgoods_15'
 
-SPIDER_MODULES = ['topgoods_11.spiders']
-NEWSPIDER_MODULE = 'topgoods_11.spiders'
+SPIDER_MODULES = ['topgoods.spiders']
+NEWSPIDER_MODULE = 'topgoods.spiders'
 
 DOWNLOADER_MIDDLEWARES = {
         'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware':301,
