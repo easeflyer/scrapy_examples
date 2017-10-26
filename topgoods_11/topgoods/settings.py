477772c8291ea70209f4a7dc9b4d3500b197e63a
jian zhang <jian.zhang@fuwo.com>
2017-10-26 13:38:21 +0800

    init;

---
+++ topgoods_11/topgoods/settings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for topgoods_11 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'topgoods_11'
+
+SPIDER_MODULES = ['topgoods_11.spiders']
+NEWSPIDER_MODULE = 'topgoods_11.spiders'
+
+DOWNLOADER_MIDDLEWARES = {
+        'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware':301,
+    }
+    
+LOG_FILE = "scrapy.log"
